8884bae6dbfcac3116c9da6c7719376d8f874622
Swaroop-bhat <swaroopbhat1234@gmail.com>
2026-09-01 19:33:44 +0530

    Code push

---
+++ Basic/2011.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        res = 0
+        for op in operations:
+            if "+" in op:
+                res += 1
+            elif "-" in op:
+                res -= 1
+            else:
+                continue
+        return res
