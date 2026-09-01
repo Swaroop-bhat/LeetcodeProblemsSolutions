0f6202b0d0645faa801cda1cb323cba1f9291759
Swaroop-bhat <swaroopbhat1234@gmail.com>
2026-09-01 19:50:32 +0530

    code push

---
+++ Basic/1947.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        count = 0
+        for ch in patterns:
+            if ch in word:
+                count += 1
+        return count
